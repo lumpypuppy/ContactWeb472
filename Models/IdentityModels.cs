7a1006576978d7c1bb5328d7ee51036186ee6027
Christopher Hernandez <structuredmethods@gmail.com>
2020-02-07 13:09:42 -0500

    Added security to Contacts

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,7 @@ namespace ContactWeb472.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public virtual List<Contact> Contacts { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +22,7 @@ namespace ContactWeb472.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        
         public DbSet<State> States { get; set; }
         public DbSet<Contact> Contacts { get; set; }
 
